13ad16203436d1ad5389a53e89c13228b7d4b353
nirbhunepal <nirbhayanepal@gmail.com>
2025-07-09 14:25:00 -0400

    sorting numbers quick sort

---
+++ quicksort.py
@@ -0,0 +1,20 @@
+nums = [6, 7, 1, 3, 9, 5]
+
+
+def sort(nums):
+    first = nums[0]
+    left = []
+    right = []
+    for i in nums[1:]:
+        if i <= first:
+            left.append(i)
+        else:
+            right.append(i)
+            
+    return sort(left)+[first]+sort(right)
+    
+    
+    
+    
+a = sort(nums)
+print(a)
